ff9e69181f6f8de89fb8ff9e74b652fd401e0301
Boyn Francis <boynfrancis@gmail.com>
2025-04-15 11:47:46 +0530

    Create 1. 73. Set Matrix Zeroes.py

---
+++ Day 1 : Arrays/Python/1. 73. Set Matrix Zeroes.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/set-matrix-zeroes/description/
+class Solution:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        col0 = 1  # To track if the first column needs to be zeroed
+        rows, cols = len(matrix), len(matrix[0])
+
+        # First pass: use matrix[i][0] and matrix[0][j] as flags
+        for i in range(rows):
+            if matrix[i][0] == 0:
+                col0 = 0  # First column will need to be zeroed
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0  # Mark the row
+                    matrix[0][j] = 0  # Mark the column
+
+        # Second pass: apply zeros based on flags, in reverse to avoid overwriting markers
+        for i in range(rows - 1, -1, -1):
+            for j in range(cols - 1, 0, -1):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+            if col0 == 0:
+                matrix[i][0] = 0
